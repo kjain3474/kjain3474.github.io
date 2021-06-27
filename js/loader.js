01e79532eff3816ffb77df4db51ee728206d54df
Kapil Jain <kjain3474@gmail.com>
2021-06-27 23:08:39 +0530

    fingerprint

--- js/loader.js
+++ js/loader.js
@@ -1,4 +1,9 @@
 $(window).load(function() {
-    console.log("loded")
-    $(".loader").fadeOut(3000);
+    setTimeout(function(){  
+        $(".loader").fadeOut(3000);
+     }, 3000);
+     setTimeout(function(){  
+        $(".fingerprint").fadeIn(3000);
+     }, 5000);
+     
 });
