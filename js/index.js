b64516bce9310afcc21fc75a2cd59be1263cb44e
Kapil Jain <kjain3474@gmail.com>
2021-07-04 21:08:28 +0530

    color change

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $("#fingerprint").click(function(){
+    $("#fingerprint").hover(function(){
         $(".fingerprint").fadeOut(3000);
         setTimeout(function(){  
             $("#main").fadeIn(3000);
