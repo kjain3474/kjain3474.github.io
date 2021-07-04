683bc94be2d4f547b8ae7f785b544538b3ee2cd4
Kapil Jain <kjain3474@gmail.com>
2021-07-04 21:32:34 +0530

    finger print scanner timing

--- js/index.js
+++ js/index.js
@@ -1,8 +1,19 @@
 $(document).ready(function() {
-    $("#fingerprint").click(function(){
-        $(".fingerprint").fadeOut(3000);
-        setTimeout(function(){  
-            $("#main").fadeIn(3000);
-         }, 3000);    
+    $t = 1;
+    $("#fingerprint").hover(function(){
+        timeout = setTimeout(function(){
+            $t = 0;
+            $(".fingerprint").fadeOut(3000);
+            setTimeout(function(){  
+                $("#main").fadeIn(3000);
+            }, 3000)
+        }, 200)
+    }, function(){
+        console.log("hover, out", $t)
+        if($t == 1){
+            clearTimeout(timeout)
+        }else{
+            $t = 1;
+        }
     });
 });
